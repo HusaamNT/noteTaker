cdd2021caa1281609be1f89a9af02335d62b25ef
Husaam NT <husnaeem@gmail.com>
2022-12-09 15:17:21 +0000

    added main page and . notes, additionally the app.listen, adjusted package.json files

--- server.js
+++ server.js
@@ -10,3 +10,28 @@ app.use(express.urlencoded({ extended:true }));
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
+);
+
+app.get('/notes', (req, res)=>
+    res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
+);
+// WHEN I click on the link to the notes page
+// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+
+// WHEN I enter a new note title and the note’s text
+// THEN a Save icon appears in the navigation at the top of the page
+
+// WHEN I click on the Save icon
+// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
+
+// WHEN I click on an existing note in the list in the left-hand column
+// THEN that note appears in the right-hand column
+
+// WHEN I click on the Write icon in the navigation at the top of the page
+// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
+
+app.listen(PORT, () => 
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
