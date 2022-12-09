e69a5ac1de96a4eb4cb551444cda48230e1ad2b3
Husaam NT <husnaeem@gmail.com>
2022-12-09 14:48:11 +0000

    added port no and midleware

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const path = require('path');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+
+app.use(express.static('public'));
+
