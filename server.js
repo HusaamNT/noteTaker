3c905107beeb871229e1422d04e0512015cb3617
Husaam NT <husnaeem@gmail.com>
2022-12-26 14:48:00 +0000

    cleaned code and completed delete function

--- server.js
+++ server.js
@@ -1,72 +1,59 @@
-const express = require('express');
-const path = require('path');
-const db = require('./db/db.json')
-const fs = require("fs")
-const {v4 : uuid} = require("uuid")
+const express = require("express");
+const path = require("path");
+const db = require("./db/db.json");
+const fs = require("fs");
+const { v4: uuid } = require("uuid");
+const { json } = require("express");
 
 const PORT = 3001;
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended:true }));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, './public/index.html'))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/index.html"))
 );
 
-app.get('/notes', (req, res)=>
-    res.sendFile(path.join(__dirname, './public/notes.html'))
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
 );
-app.post('/husaam/:job/:food',(req, res)=>{
+app.post("/husaam/:job/:food", (req, res) => {
   console.log(req.params);
-  res.json('yes he is!');
-})
-app.get('/api/notes', (req, res)=>{
+  res.json("yes he is!");
+});
+app.get("/api/notes", (req, res) => {
   res.json(db);
-}
-)
+});
 
-app.post('/api/notes', (req, res)=>{
+app.post("/api/notes", (req, res) => {
   const newNote = {
     title: req.body.title,
     text: req.body.text,
-    id: uuid()
-  }
+    id: uuid(),
+  };
   db.push(newNote);
-  fs.writeFile("./db/db.json", JSON.stringify(db), err => console.log(err))
+  fs.writeFile("./db/db.json", JSON.stringify(db), (err) => console.log(err));
   res.json(db);
-})
+});
 
-fsDelete = (data, param) =>{
-  console.log("hello from fsDelete!")
-  const array = JSON.parse(data);
-  for (let i=0; i<array.length; i++){
-  //console.log(array[i].id)
-  if (array[i].id === param){
-    const newData = array.replace(array[i], '')
-    fs.writeFile("./db/db.json", newData, 'utf-8')
-  }else{
-    console.log("Cannot find ID")
-  }
-  }
- // const detectId = array.id
-  //console.log(detectId)
-  //const newData = data.replace(detectId, '')
-  //fs.writeFile("./db/db.json", newData, 'utf-8')
-};
-//TODO: fix delete functionnid
-app.delete('/api/notes/:id', (req, res)=>{
-  const param = req.param.id
-  console.log(param)
-  fs.readFile("./db/db.json", "utf8", (err, data) => {
-      console.log(data, param);
-      fsDelete(data, param)
- })
+app.delete("/api/notes/:id", (req, res) => {
+  const param = req.params.id;
 
-})
+  const newArray = [];
+  for (let i = 0; i < db.length; i++) {
+    if (db[i].id !== param) {
+      newArray.push(db[i]);
+    }
+  }
+  fs.writeFile("./db/db.json", JSON.stringify(newArray), (err) => {
+    err ? console.log(err) : res.json(newArray);
+  });
+});
+
 // WHEN I click on the link to the notes page
 // THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
 
@@ -82,7 +69,7 @@ app.delete('/api/notes/:id', (req, res)=>{
 // WHEN I click on the Write icon in the navigation at the top of the page
 // THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
 
-app.listen(PORT, () => 
+app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
 
